14d344cd87f2de8bec88b5e05331c31d237af6fd
AaronKurian <aaronkurianabraham@gmail.com>
2024-08-04 17:51:31 +0530

    personalise

--- portfolio/src/components/Intro/intro.jsx
+++ portfolio/src/components/Intro/intro.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import {Link} from 'react-scroll';
 import './intro.css';
-import pic from '../../../src/assets/pic.png'
+import pic2 from '../../../src/assets/pic2.png'
 import hireme from '../../../src/assets/hireme.png'
 const Intro = () => {
   return (
     <section id='intro'>
         <div className='introContent'>
 <span className="hello">Hello,</span>
-<span className="introText">I'm <span className="introName">Smith</span> <br />Website Designer</span>
-      <p className="introPara">Im\'m a skilled Web Designer with experience in creating <br />visually appealing and user friendly website</p> 
+<span className="introText">I'm <span className="introName">Aaron</span> <br />Website Designer</span>
+      <p className="introPara">I'm a skilled Web Designer with experience in creating <br />visually appealing and user friendly website</p> 
+      <Link><button className="btn"><img src={hireme} alt="Hire" className='btnImg' />Resume</button></Link>
         </div>
-        <Link><button className="btn"><img src={hireme} alt="Hire" className='btnImg' />Hire Me</button></Link><img src={pic} alt="Profile" className='bg'/>
+      
+        {/* <Link><button className="btn"><img src={hireme} alt="Hire" className='btnImg' />Resume</button></Link> */}
+        <img src={pic2} alt="Profile" className='bg'/>
+        
     </section>
   )
 }
--- portfolio/src/components/Navbar/navbar.jsx
+++ portfolio/src/components/Navbar/navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import "./navbar.css"
 // import logo from '../../../public/logo.png'
  import contact from '../../../src/assets/contact.png'
+ import logo4 from '../../../src/assets/logo4.png'
+ import menurounded from '../../../src/assets/menurounded.svg'
 import {Link} from 'react-scroll';
 
 const Navbar= () => {
@@ -9,7 +11,7 @@ const[showMenu, setShowMenu]= useState(false);
 
   return (
     <nav className='navbar'>
-        <img src='' alt="" className='logo' />
+        <img src={logo4} alt="" className='logo' />
         <div className="desktopMenu">
             <Link activeClass='active' to='intro' spy={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
             <Link activeClass='active' to='skills' spy={true} offset={-50} duration={500} className='desktopMenuListItem'>About</Link>
@@ -22,7 +24,7 @@ const[showMenu, setShowMenu]= useState(false);
             <img src={contact} alt="" className='desktopMenuImg' />Contact Me
         </button>
 
-        <img src='' alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)} />
+        <img src={menurounded} alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)} />
         <div className="navMenu" style={{display: showMenu?'flex':'none'}}>
             <Link activeClass='active' to='intro' spy={true} offset={-100} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Home</Link>
             <Link activeClass='active' to='skills' spy={true} offset={-50} duration={500} className='listItem'onClick={()=>setShowMenu(false)}>About</Link>
